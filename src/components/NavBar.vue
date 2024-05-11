d762ce07ecef92414da6cbfd715cb6dc1d8c1f8d
Nathan Hollows <me@nathanhollows.com>
2024-05-12 11:50:05 +1200

    feat: new logo in nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavLogo from './logos/NavLogo.vue'
+</script>
 <template>
   <nav class="bg-orbus-500 p-4 text-white">
     <div class="container mx-auto align-middle flex justify-between md:max-w-xl">
-      <h1 class="text-lg">Crystal Orbus</h1>
+      <NavLogo class="h-8 self-center" />
       <div class="flex gap-3">
-        <RouterLink to="/" class="text-white p-2 hover:bg-orbus-700 rounded font-bold"
+        <RouterLink
+          to="/"
+          class="text-white p-2 hover:bg-orbus-900 transition-all rounded font-bold"
           >My Stops</RouterLink
         >
-        <RouterLink to="/map" class="text-white p-2 hover:bg-orbus-700 rounded font-bold"
+        <RouterLink
+          to="/map"
+          class="text-white p-2 hover:bg-orbus-900 transition-all rounded font-bold"
           >Map</RouterLink
         >
       </div>
